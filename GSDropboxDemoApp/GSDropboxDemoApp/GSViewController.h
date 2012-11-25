ec5f5dbbcf11ca561cd1e98758cdac9a5b3e3bb2
Simon Whitaker <simon@goosoftware.co.uk>
2012-11-25 13:36:40 +0000

    Initial commit of demo app

---
+++ GSDropboxDemoApp/GSDropboxDemoApp/GSViewController.h
@@ -0,0 +1,13 @@
+//
+//  GSViewController.h
+//  GSDropboxDemoApp
+//
+//  Created by Simon Whitaker on 25/11/2012.
+//  Copyright (c) 2012 Goo Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GSViewController : UIViewController
+
+@end
---
+++ GSDropboxDemoApp/GSDropboxDemoApp/GSViewController.m
@@ -0,0 +1,29 @@
+//
+//  GSViewController.m
+//  GSDropboxDemoApp
+//
+//  Created by Simon Whitaker on 25/11/2012.
+//  Copyright (c) 2012 Goo Software Ltd. All rights reserved.
+//
+
+#import "GSViewController.h"
+
+@interface GSViewController ()
+
+@end
+
+@implementation GSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
